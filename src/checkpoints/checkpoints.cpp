d2f97fed6ed7be719b4712663563ba3c9e8fea3e
John D <az0006t@protonmail.com>
2025-04-06 22:18:51 -0400

    Update checkpoints.cpp

--- src/checkpoints/checkpoints.cpp
+++ src/checkpoints/checkpoints.cpp
@@ -198,7 +198,7 @@ namespace cryptonote
       return true;
     }
     ADD_CHECKPOINT2(0,     "273c147ee3a5652180d33268a77c9274c6ef29df67cb8b352327599f07799562", "0x1");
-    ADD_CHECKPOINT2(2,     "16ef3267910a8c40aac4532528b6a4d0905898b5c7bf24da606e6c380362d1a7", "0x2");
+    ADD_CHECKPOINT2(2,     "20f782372b13c39bf23dfd8b4ac4620af4192402a118b1a932f4cf365a2aeaa2", "0x2");
     return true;
   }
 
