6360f643e7a3afef8f138370e657461f9825b220
John D <az0006t@protonmail.com>
2025-05-17 19:58:12 -0400

    Update cryptonote_tx_utils.cpp

--- src/cryptonote_core/cryptonote_tx_utils.cpp
+++ src/cryptonote_core/cryptonote_tx_utils.cpp
@@ -82,18 +82,31 @@ namespace cryptonote
     tx.extra.clear();
 
     keypair txkey = keypair::generate(hw::get_device("default"));
-    add_tx_pub_key_to_extra(tx, txkey.pub);
-//    if(!extra_nonce.empty())
-//      if(!add_extra_nonce_to_tx_extra(tx.extra, extra_nonce))
-//        return false;
 
-if (!extra_nonce.empty())
-{
-  // Use custom 0xFA tag instead of TX_EXTRA_NONCE
-  tx.extra.push_back(0xFA);  // custom tag
-  tx.extra.push_back(static_cast<uint8_t>(extra_nonce.size()));  // length byte (must be ≤ 255)
-  tx.extra.insert(tx.extra.end(), extra_nonce.begin(), extra_nonce.end());
-}
+
+    size_t reserve_size = extra_nonce.size();
+    fprintf(stderr, "Requested reserve size: %zu\n", extra_nonce.size());
+
+    if (reserve_size < 250) { 
+      add_tx_pub_key_to_extra(tx, txkey.pub);
+    }
+
+    if (reserve_size < 250) {
+      (stderr, "Including tx_pub_key (reserve size %zu)\n", reserve_size);
+      add_tx_pub_key_to_extra(tx, txkey.pub);
+    } else {
+      fprintf(stderr, "Skipping tx_pub_key to preserve space (reserve size %zu)\n", reserve_size);
+    }
+
+
+
+
+
+    
+    if(!extra_nonce.empty())
+      if(!add_extra_nonce_to_tx_extra(tx.extra, extra_nonce))
+        return false;
+
     if (!sort_tx_extra(tx.extra, tx.extra))
       return false;
 
