a9e668ec8f157f7ea93d12fbec646d7c89e4792a
John D <az0006t@protonmail.com>
2025-06-08 19:26:28 -0400

    Update simplewallet.cpp

--- src/simplewallet/simplewallet.cpp
+++ src/simplewallet/simplewallet.cpp
@@ -3478,6 +3478,8 @@ bool simple_wallet::delegate_register(const std::vector<std::string>& args)
 
   data2 = final.str();
 
+  INITIALIZE_NETWORK_DATA_NODES_LIST;
+
   int seed_count = 0;
   for (int count = 0; count < total_delegates; ++count) {
     if (std::find(network_data_nodes_list.begin(), network_data_nodes_list.end(), block_verifiers_IP_address[count]) != network_data_nodes_list.end()) {
