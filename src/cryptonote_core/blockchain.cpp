0812df7b5a8f0926926aa6bcea334d671efad031
John D <az0006t@protonmail.com>
2025-07-02 12:10:50 -0400

    Update blockchain.cpp

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -4093,13 +4093,13 @@ for (size_t i = 0; i < bl.miner_tx.extra.size(); ++i) {
   fprintf(stderr, "%02x ", bl.miner_tx.extra[i]);
   if ((i + 1) % 16 == 0) fprintf(stderr, "\n");
 }
-//fprintf(stderr, "\n");
-//std::vector<cryptonote::tx_extra_field> extra_fields;
-//if (!cryptonote::parse_tx_extra(bl.miner_tx.extra, extra_fields)) {
-//    MERROR_VER("Failed to parse tx_extra in block id: " << id);
+fprintf(stderr, "\n");
+std::vector<cryptonote::tx_extra_field> extra_fields;
+if (!cryptonote::parse_tx_extra(bl.miner_tx.extra, extra_fields)) {
+    MERROR_VER("Failed to parse tx_extra in block id: " << id);
 //    bvc.m_verifivation_failed = true;
 //    goto leave;
-//}
+}
 
 
 
