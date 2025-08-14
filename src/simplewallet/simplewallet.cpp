c97b4d3a593f25271fe0eb60c5a821e6be7f2c27
John D <az0006t@protonmail.com>
2025-08-13 23:08:54 -0400

    Update simplewallet.cpp

--- src/simplewallet/simplewallet.cpp
+++ src/simplewallet/simplewallet.cpp
@@ -3609,7 +3609,7 @@ bool simple_wallet::delegate_register(const std::vector<std::string>& args)
       rbuffer = send_and_receive_data(host, senddata, SEND_OR_RECEIVE_SOCKET_DATA_TIMEOUT_SETTINGS * 2);
 
       bool ok = parse_dpops_response(rbuffer, status_text);
-      // std::cout << "[DEBUG] host=" << host << " rbuffer=" << rbuffer << std::endl;
+      std::cout << "[DEBUG] host=" << host << " rbuffer=" << rbuffer << std::endl;
       if (ok) {
         ++reply_count;
       }
@@ -3619,7 +3619,7 @@ bool simple_wallet::delegate_register(const std::vector<std::string>& args)
     bool local_ok = false;
     rbuffer = send_and_receive_data("127.0.0.1", senddata, SEND_OR_RECEIVE_SOCKET_DATA_TIMEOUT_SETTINGS * 2);
     local_ok = parse_dpops_response(rbuffer, status_text);
-    std::cout << "[DEBUG] host=" << host << " rbuffer=" << rbuffer << std::endl;
+    std::cout << "[DEBUG] host=localhost rbuffer=" << rbuffer << std::endl;
 
     if (reply_count >= total_delegates_valid_amount) {
       message_writer(console_color_green, false) << "The delegate has been registered successfully";
