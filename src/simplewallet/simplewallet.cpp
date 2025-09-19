7710ed09fc768b85e86bbe1e2650b616a41057a3
John D <az0006t@protonmail.com>
2025-09-19 18:00:28 -0400

    Update simplewallet.cpp

--- src/simplewallet/simplewallet.cpp
+++ src/simplewallet/simplewallet.cpp
@@ -4278,6 +4278,7 @@ bool simple_wallet::vote_status(const std::vector<std::string> &args) {
 
     host = network_data_nodes_list[idx];
     rbuffer = send_and_receive_data(host.c_str(), senddata, SEND_OR_RECEIVE_SOCKET_DATA_TIMEOUT_SETTINGS);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     status_text.clear();
     ok = parse_dpops_response(rbuffer, status_text);
 
