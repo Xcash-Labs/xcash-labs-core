74c2e91a687f5f145a1ddfa5668bd37ccfa45485
John D <az0006t@protonmail.com>
2025-10-27 18:40:30 -0400

    Update miner.cpp

--- src/cryptonote_basic/miner.cpp
+++ src/cryptonote_basic/miner.cpp
@@ -88,96 +88,37 @@ using namespace epee;
 
 extern "C" void slow_hash_allocate_state();
 extern "C" void slow_hash_free_state();
-namespace cryptonote
-{
+namespace cryptonote {
 
-  namespace
-  {
-    const command_line::arg_descriptor<std::string> arg_extra_messages =  {"extra-messages-file", "Specify file for extra messages to include into coinbase transactions", "", true};
-    const command_line::arg_descriptor<std::string> arg_start_mining =    {"start-mining", "Specify wallet address to mining for", "", true};
-    const command_line::arg_descriptor<uint32_t>      arg_mining_threads =  {"mining-threads", "Specify mining threads count", 0, true};
-    const command_line::arg_descriptor<bool>        arg_bg_mining_enable =  {"bg-mining-enable", "enable background mining", true, true};
-    const command_line::arg_descriptor<bool>        arg_bg_mining_ignore_battery =  {"bg-mining-ignore-battery", "if true, assumes plugged in when unable to query system power status", false, true};    
-    const command_line::arg_descriptor<uint64_t>    arg_bg_mining_min_idle_interval_seconds =  {"bg-mining-min-idle-interval", "Specify min lookback interval in seconds for determining idle state", miner::BACKGROUND_MINING_DEFAULT_MIN_IDLE_INTERVAL_IN_SECONDS, true};
-    const command_line::arg_descriptor<uint16_t>     arg_bg_mining_idle_threshold_percentage =  {"bg-mining-idle-threshold", "Specify minimum avg idle percentage over lookback interval", miner::BACKGROUND_MINING_DEFAULT_IDLE_THRESHOLD_PERCENTAGE, true};
-    const command_line::arg_descriptor<uint16_t>     arg_bg_mining_miner_target_percentage =  {"bg-mining-miner-target", "Specify maximum percentage cpu use by miner(s)", miner::BACKGROUND_MINING_DEFAULT_MINING_TARGET_PERCENTAGE, true};
-  }
+namespace {
+const command_line::arg_descriptor<std::string> arg_extra_messages = {"extra-messages-file", "Specify file for extra messages to include into coinbase transactions", "", true};
+const command_line::arg_descriptor<std::string> arg_start_mining = {"start-mining", "Specify wallet address to mining for", "", true};
+const command_line::arg_descriptor<uint32_t> arg_mining_threads = {"mining-threads", "Specify mining threads count", 0, true};
+const command_line::arg_descriptor<bool> arg_bg_mining_enable = {"bg-mining-enable", "enable background mining", true, true};
+const command_line::arg_descriptor<bool> arg_bg_mining_ignore_battery = {"bg-mining-ignore-battery", "if true, assumes plugged in when unable to query system power status", false, true};
+const command_line::arg_descriptor<uint64_t> arg_bg_mining_min_idle_interval_seconds = {"bg-mining-min-idle-interval", "Specify min lookback interval in seconds for determining idle state", miner::BACKGROUND_MINING_DEFAULT_MIN_IDLE_INTERVAL_IN_SECONDS, true};
+const command_line::arg_descriptor<uint16_t> arg_bg_mining_idle_threshold_percentage = {"bg-mining-idle-threshold", "Specify minimum avg idle percentage over lookback interval", miner::BACKGROUND_MINING_DEFAULT_IDLE_THRESHOLD_PERCENTAGE, true};
+const command_line::arg_descriptor<uint16_t> arg_bg_mining_miner_target_percentage = {"bg-mining-miner-target", "Specify maximum percentage cpu use by miner(s)", miner::BACKGROUND_MINING_DEFAULT_MINING_TARGET_PERCENTAGE, true};
+}  // namespace
 
-  void miner::resume()
-  {
-
-  }
-
-  bool miner::stop()
-  {
-    return true;
-  }
-//-----------------------------------------------------------------------------------------------------
-  bool miner::is_mining() const
-  {
-    return false;
-  }
-  //-----------------------------------------------------------------------------------------------------
-  bool miner::set_block_template(const block& bl, const difficulty_type& di, uint64_t height, uint64_t block_reward)
-  {
-    CRITICAL_REGION_LOCAL(m_template_lock);
-    m_template = bl;
-    m_diffic = di;
-    m_height = height;
-    m_block_reward = block_reward;
-    ++m_template_no;
-    m_starter_nonce = crypto::rand<uint32_t>();
-    return true;
-  }
-  //-----------------------------------------------------------------------------------------------------
-  bool miner::on_block_chain_update()
-  {
-    if(!is_mining())
-      return true;
-
-    return request_block_template();
-  }
-  //-----------------------------------------------------------------------------------------------------
-  bool miner::request_block_template()
-  {
-    block bl;
-    difficulty_type di = AUTO_VAL_INIT(di);
-    uint64_t height = AUTO_VAL_INIT(height);
-    uint64_t expected_reward; //only used for RPC calls - could possibly be useful here too?
-
-    cryptonote::blobdata extra_nonce;
-    if(m_extra_messages.size() && m_config.current_extra_message_index < m_extra_messages.size())
-    {
-      extra_nonce = m_extra_messages[m_config.current_extra_message_index];
-    }
-
-    uint64_t cumulative_weight;
-    uint64_t seed_height;
-    crypto::hash seed_hash;
-    if(!m_phandler->get_block_template(bl, m_mine_address, di, height, expected_reward, cumulative_weight, extra_nonce, seed_height, seed_hash))
-    {
-      LOG_ERROR("Failed to get_block_template(), stopping mining");
-      return false;
-    }
-    set_block_template(bl, di, height, expected_reward);
-    return true;
-  }
-//-----------------------------------------------------------------------------------------------------
-  bool miner::find_nonce_for_given_block(const get_block_hash_t &gbh, block& bl, const difficulty_type& diffic, uint64_t height, const crypto::hash *seed_hash)
-  {
-    for(; bl.nonce != std::numeric_limits<uint32_t>::max(); bl.nonce++)
-    {
-      crypto::hash h;
-      gbh(bl, height, seed_hash, diffic <= 100 ? 0 : tools::get_max_concurrency(), h);
-
-      if(check_hash(h, diffic))
-      {
-        bl.invalidate_hashes();
-        return true;
-      }
-    }
-    bl.invalidate_hashes();
-    return false;
-  }
-
+void miner::init_options(boost::program_options::options_description& desc) {
+  return;
 }
+
+bool miner::on_idle() {
+  return true;
+}
+
+void miner::resume() {
+  return;
+}
+
+bool miner::stop() {
+  return true;
+}
+//-----------------------------------------------------------------------------------------------------
+bool miner::is_mining() const {
+  return false;
+}
+
+}  // namespace cryptonote
